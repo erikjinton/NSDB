b4e6453fc384021d4c6254e6e983bb5f11506353
erikjinton <erik.jinton@gmail.com>
2016-10-13 16:38:58 +0200

    rörelse+hinder

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 // pl = player
 var plY = 520;
+var plX = 200;
 var plVY = 0;
+
 var ctx;
+
+var h1X = 730;
+var h1Y = 570;
+var h1VX = 0;
+var h1VY = 0;
+
 
 function init() {
     
@@ -13,38 +21,64 @@ function init() {
     //createjs.Sound.registerSound("ljud/horn.mp3", "mySound");
 }
 
+
 function update() {
     
     //Suddar
     ctx.clearRect(0, 0, 900, 600);
     
-    //Målar ut Spelare
-    ctx.fillRect(200, plY, 25, 80);
+    //Målar ut Spelare (pl)
+    ctx.fillRect(plX, plY, 25, 80);
+    
+    //Målar ut hinder1 (h1)
+    ctx.fillRect(h1X, h1Y, 30, 30);
     
     //Hoppa
     plY = plY + plVY;
     
-    //Stanna plY mot underkanti
+    //Rörelse
+    h1X = h1X + h1VX;
+    
+    //Stanna plY mot underkant
     if(plY >= 520) {
         plVY = 0;
     }
     
-    //Hopp-limit
-    if((plY )
+    //Stanna mot överkant
+    if(plY <= 0) {
+        plY = 0;
+    }
+    
+    //Kollission med h1
+    //if((plX >= h1X) && (plX <= h1X + 30) (plX >= h1Y + 50) (plY <= h1Y + 30)) {
+    //    h1VX = 0;
+    //}
 }
+
 
 function keyDown(e) {
     
     //Piltangent upp = Hoppa
     if(e.keyCode == 38) {
-        plVY = -13;
+        plVY = -18;
+    }
+    
+    //Rörelse fram
+    if(e.keyCode == 39) {
+        h1VX = -13;
     }
 }
+
 
 function keyUp(e) {
     
     //Släpp piltangent upp = Fall
     if(e.keyCode == 38) {
-        plVY = 10;
+        plVY = 18;
+    }
+    
+    //Stanna
+    if(e.keyCode == 39) {
+        h1VX = 0;
     }
 }
